0c43cdff89745b4d45baa2d8c0546336e6ef5a19
AndreaQuinCa <andrea.quintanilla@cimat.mx>
2020-10-09 21:24:45 -0500

    Prueba Hola

--- esteban-andrea/main.c
+++ esteban-andrea/main.c
@@ -4,9 +4,16 @@
 #include "./median.c"
 
 // Función principal, recibe de STDIN un stream de datos e imprime a STDOUT
+<<<<<<< Updated upstream
 // la mediana. 
 //
 int main(int num_args, char ** args){ 
+=======
+// la mediana.
+
+//prueba hola
+int main(int num_args, char ** args){
+>>>>>>> Stashed changes
   // TODO
   return 0;
 } 
