6c99edf5b36dbcbe9db9c6f09f01ffd61dbe2809
1MarcoFlores <59987236+1MarcoFlores@users.noreply.github.com>
2020-10-09 22:43:39 -0500

    Update heap.c
    Agregué la función topDownHeapify

--- javier.g-ricardo.n/heap.c
+++ javier.g-ricardo.n/heap.c
@@ -35,8 +35,24 @@ void bottomUpHeapify(int *arr, int k){
   return;
 } 
 
-void topDownHeapify(int * arr, int k, int n){ 
-  return;
+void topDownHeapify(int * arr, int k, int n){
+  while(3*k-1 <= n){
+		int j = 3*k - 1;//primer nodo hijo
+		//Compara con los nodos hijos
+		if(j<n && arr[j]<arr[j+1]){// j<n para j++
+			j++;
+		}
+		else if(j<n-1 && arr[j]<arr[j+2]){//j<n-1 para j = j+2
+			j += 2;
+		}
+		
+		//Si el nodo padre es mayor que el nodo hijo mayor, se detiene
+		if(arr[k] >= arr[j]) break;
+		//Si el nodo padre es menor, los intercambia.
+		swap(arr, k, j);
+		//Actualiza k con el nodo hijo que acaba de cambiar
+		k = j;
+	}
 } 
 
 #endif
