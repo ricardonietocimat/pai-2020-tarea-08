9f5beba8f9d6ce085fdce2b924651c0ac7fea952
ivonne.monter <ivonne.monteraldana@gmail.com>
2020-10-09 19:49:05 -0500

    primeras funciones de la estructura

---
+++ isabel.l-ivonne.m/heap.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "heap.h"
+// Definiciones ===============================================================
+THeap* THeap_new(){
+	// Asignación de memoria a la  struct THeap.
+	THeap* heap = (THeap*)malloc(sizeof(THeap));
+	return heap;
+	
+}
+
+void free_THeap(THeap** hptr){
+	free((*hptr)->data);
+	free(*hptr);
+}
+
+void swap(int *arr, int i, int j){
+	int tmp= arr[i];
+	arr[i] = arr[j];
+	arr[j] = tmp;
+}
+
+void insert(THeap *h, int data){ 
+	
+  if(h->n+1 == h->capacity) return;
+  h->data[++(h->n)]= data;
+  bottomUpHeapify(h->data, h->n);
+  return;
+  
+} 
+
+int removeMax(THeap *h){ 
+  return -1;
+} 
+
+int getMax(THeap *h){ 
+  return -1;
+} 
+
+void bottomUpHeapify(int *arr, int k){ 
+  while (k > 1 && arr[(k-1)/3]<arr[k]){
+	swap(arr,k,(k-1)/3);
+	k=(k-1)/3;
+	}
+  return;
+} 
+
+void topDownHeapify(int * arr, int k, int n){ 
+  return;
+} 
+
---
+++ isabel.l-ivonne.m/heap.h
@@ -0,0 +1,21 @@
+#ifndef HEAP_T08
+#define HEAP_T08
+// Prototipos =================================================================
+typedef struct _THeap{
+	unsigned int capacity ;
+	unsigned int n;
+	int* data;
+}THeap;
+
+THeap * THeap_new(void);
+void    free_THeap(THeap ** hptr);
+
+void    insert(THeap *h, int data);
+int     removeMax(THeap *h);
+int     getMax(THeap *h);
+
+void    bottomUpHeapify(int *arr, int k);
+void    topDownHeapify(int * arr, int k, int n);
+
+void swap(int *arr, int i, int j);
+#endif
