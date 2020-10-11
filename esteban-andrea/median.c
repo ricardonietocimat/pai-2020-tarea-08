a24bcad3d76b734b52639ae857e441f5191d7d11
AndreaQuinCa <andrea.quintanilla@cimat.mx>
2020-10-11 01:15:34 -0500

    primera versión theap_median
    Falta decidir lo de la implementación del montículo min y modificar las veces en donde se llama

--- esteban-andrea/median.c
+++ esteban-andrea/median.c
@@ -4,13 +4,62 @@
 // Prototipo ==================================================================
 
 int theap_median(THeap * thmin, THeap * thmax, int data);
- 
+
 
 // Definiciones ===============================================================
 
-int theap_median(THeap * thmin, THeap * thmax, int data){ 
-  return -1;
-} 
+double theap_median(THeap * thmin, THeap * thmax, int data){
+
+    int min_sze = thmin->size;
+    int max_sze = thmax->size;
+    if(abs(min_sze-max_sze)>1){
+        printf("El tamaño de los monticulos difiere por mas de uno\n");
+        return -1;
+    }
+    double med = 0; //Valor actual del mediano
+    if(max_sze>min_sze){
+        med = (double)getMax(thmax);
+        if(data<med){
+            removeMax(thmax);
+            insert(thmin, med);
+            insert(thmax, data);
+        }
+        else{
+            insert(thmin, data);
+        }
+
+        return (getMax(thmin)+getMax(thmax))/2.0;
+
+    }
+    else if(min_sze>max_sze){
+        med = getMax(thmin);
+        if(data>med){
+            removeMax(thmin);
+            insert(thmax, med);
+            insert(thmin, data);
+        }
+        else{
+            insert(thmax, data);
+        }
+        return (getMax(thmin)+getMax(thmax))/2.0;
+    }
+    else{
+        med = (getMax(thmin)+getMax(thmax))/2.0;
+        if(data<med){
+            insert(thmax, data);
+            return (double)getMax(thmax);
+        }
+        else{
+            insert(thmin, data);
+            return (double)getMax(thmin);
+        }
 
 
-#endif 
+    }
+
+
+    return -1;
+}
+
+
+#endif
