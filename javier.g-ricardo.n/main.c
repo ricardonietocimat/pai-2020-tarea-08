fb1863d10731c5538b236d6ba5223347568ef63a
Ntl21hub <70297372+Ntl21hub@users.noreply.github.com>
2020-10-12 13:32:23 -0500

    Update main.c

--- javier.g-ricardo.n/main.c
+++ javier.g-ricardo.n/main.c
@@ -7,6 +7,23 @@
 // la mediana. 
 //
 int main(int num_args, char ** args){ 
-  // TODO
+    int num=0;
+    float med=0;
+    THeap * thmin=NULL,* thmax=NULL;
+    FILE *arch.*arch2;
+    arch = stdin;
+    arch2 = stdout;
+    if(arch!=NULL && arch2!=NULL){
+        while( fscanf(arch,"%d ",num)>0){
+            med=theap_median(thmin, thmax, num);
+            fprintf(arch2,"%f \n",med);
+        }
+    }
+  
+  
+    free_THeap(&thmin);
+    free_THeap(&thmax)
+    fclose(arch);
+    fclose(arch2);
   return 0;
 } 
