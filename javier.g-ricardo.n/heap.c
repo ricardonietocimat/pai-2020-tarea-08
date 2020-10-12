eae32c2501ad2ea3751e07dafcf7f06d50c2eb9d
Ntl21hub <70297372+Ntl21hub@users.noreply.github.com>
2020-10-12 00:22:15 -0500

    Update heap.c

--- javier.g-ricardo.n/heap.c
+++ javier.g-ricardo.n/heap.c
@@ -12,44 +12,49 @@ typedef struct _THeap{
 THeap * THeap_new(void);
 void    free_THeap(THeap ** hptr);
 
-void    insert(THeap *h, int data);
-int     removeMax(THeap *h);
+void    insert(THeap *h, int data, char ord);
+int     removeMax(THeap *h, char ord);
 int     getMax(THeap *h);
 
-void    bottomUpHeapify(int *arr, int k);
+void    bottomUpHeapify(int *arr, int k, char ord);
+
 void    topDownHeapify(int * arr, int k, int n);
 
 // Definiciones ===============================================================
 
-void insert(THeap *h, int data){ 
-  if (h->n==h->capacity-1) 
+void insert(THeap *h, int data, char ord){
+	if (h->n==h->capacity-1) 
 		return;
 	h->data[++h->n]=data;
-	bottomUpHeapify(h->data,h->n);
-  return;
-} 
-
-int removeMax(THeap *h){ 
-  if (h->n<1) 
+	bottomUpHeapify(h->data,h->n,ord);
+}
+int removeMax(THeap *h, char ord){
+	if (h->n<1) 
 		return -1;
 	int tmp = h->data[1];
 	h->data[1]= h->data[h->n--];
-	topDownHeapify(h->data,1,h->n);
+	topDownHeapify(h->data,1,h->n,ord);
 	return tmp;
-} 
-
+}
 int getMax(THeap *h){ 
   if (h->n<1) 
     return -1;
   return h->data[1];
 } 
 
-void bottomUpHeapify(int *arr, int k){ 
-  while (k > 0 && arr[(k+1)/3]<arr[k]) {
-		swap(arr ,k, (k+1)/3);
-		k=(k+1)/3;
+void bottomUpHeapify(int *arr, int k, char ord){
+
+	if(ord==1){
+		while (k > 1 && arr[(k+1)/3]>arr[k]) {
+			swap(arr ,k, (k+1)/3);
+			k=(k+1)/3;
+		}
+	}else{
+		while (k > 0 && arr[(k+1)/3]<arr[k]) {
+			swap(arr ,k, (k+1)/3);
+			k=(k+1)/3;
+		}
 	}
-  return;
 } 
 
 void topDownHeapify(int * arr, int k, int n){ 
