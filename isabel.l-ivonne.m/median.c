c2846740a2acd1294b43b06e00d0bbcab8bcb5bc
ivonne.monter <ivonne.monteraldana@gmail.com>
2020-10-12 14:06:14 -0500

    Implementación de theapMedian

--- isabel.l-ivonne.m/median.c
+++ isabel.l-ivonne.m/median.c
@@ -1,16 +1,64 @@
-#ifndef MEDIAN_T8
-#define MEDIAN_T8
-
-// Prototipo ==================================================================
-
-int theap_median(THeap * thmin, THeap * thmax, int data);
- 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "./heap.h"
+#include "median.h"
 
 // Definiciones ===============================================================
-
-int theap_median(THeap * thmin, THeap * thmax, int data){ 
-  return -1;
+float getMedian(THeap * thmin, THeap * thmax){
+	if((thmax->n)==-1&&(thmin->n)==-1)
+		return sqrt(-1);
+	if((thmax->n)>(thmin->n)){
+			return getMax(thmax);
+		 }
+	 if((thmax->n)<(thmin->n)){
+		   return -getMax(thmin);
+		 }
+	 if(thmax->n==thmin->n){
+		 return (getMax(thmax)+(-getMax(thmin)))/2.0;
+	  }
 } 
 
+float theap_median(THeap * thmin, THeap * thmax, int data){
+	 int n = thmax->n;
+	 int m = thmin->n;
+	 float median=getMedian(thmin,thmax);
+	 if(n==m){
+		 if(median!= median)
+			insert(thmax,data);
+		 else{
+			 if(data<=median)
+				insert(thmax,data);
+			 if (data>median){
+				 insert(thmin,-data);
+				 }
+			}
+		   
+	  }
+	 if(n>m){
+				if(data<median){
+					int max = removeMax(thmax);
+					insert(thmin, -max);
+					insert(thmax,data);
+			 
+				}  
+				if(data>=median)
+					insert(thmin,-data);
+				  
+			}
+			
+	 if(n<m){
+			if(data>median){
+			  int min = -removeMax(thmin);
+			  insert(thmax,min);
+			  insert(thmin,-data);
+			 }
+			if(data<=median)
+				insert(thmax,data);
+						 
+			}
+				
+		
+	return getMedian(thmin,thmax);
+} 
 
-#endif 
---
+++ isabel.l-ivonne.m/median.h
@@ -0,0 +1,9 @@
+#ifndef MEDIAN_T8
+#define MEDIAN_T8
+
+// Prototipo ==================================================================
+
+float getMedian(THeap * thmin, THeap * thmax);
+float theap_median(THeap * thmin, THeap * thmax, int data);
+
+#endif
