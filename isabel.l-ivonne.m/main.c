183bdb4d7ed5355e8ac38b26635ad44d81328622
ivonne.monter <ivonne.monteraldana@gmail.com>
2020-10-12 14:09:26 -0500

    Se agrego la lectura del stream de datos

--- isabel.l-ivonne.m/main.c
+++ isabel.l-ivonne.m/main.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "./heap.c"
-#include "./median.c"
+#include<math.h>
+#include "./heap.h"
+#include "./median.h"
 
 // Función principal, recibe de STDIN un stream de datos e imprime a STDOUT
 // la mediana. 
 //
 int main(int num_args, char ** args){ 
-  // TODO
+  int c = 20;
+  THeap *thmin = THeap_new(c);
+  THeap *thmax = THeap_new(c);
+  int data;
+  while(scanf("%d",&data)>0){
+		printf("\n%f\n",theap_median(thmin,thmax,data));
+
+	 }
+  free_THeap(&thmin);
+  free_THeap(&thmax);
+  
   return 0;
 } 
