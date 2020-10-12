40da7275c6497055e959da874b360ee3b4ec78bc
DannaIsis <danielisisfoshiva@gmail.com>
2020-10-12 15:05:19 -0500

    actualizacion del algoritmo para calcular la media

--- javier.g-ricardo.n/median.c
+++ javier.g-ricardo.n/median.c
@@ -3,14 +3,58 @@
 
 // Prototipo ==================================================================
 
-int theap_median(THeap * thmin, THeap * thmax, int data);
+double theap_median(THeap * thmin, THeap * thmax, int data);
  
 
 // Definiciones ===============================================================
 
-int theap_median(THeap * thmin, THeap * thmax, int data){ 
+
+
+double theap_median(THeap * thmin, THeap * thmax, int data){
+    double mactual=0;
+    if(thmax->n>thmin->n){ 
+        mactual=getMax(thmax);
+        if(data<mactual){
+            insert(thmin,removeMax(thmax,0),1);
+            insert(thmax,data,0);
+        }
+            else insert(thmin,data,1);
+            mactual=(double)(getMax(thmax)+getMax(thmin))/2.0; 
+          return mactual;
+    }
+    else{
+        if(thmax->n<thmin->n){
+                  mactual=getMax(thmin);
+            if(data>mactual){
+                insert(thmax,removeMax(thmin,1),0);
+                insert(thmin,data,1);
+            }
+            else insert(thmax,data,0);
+            mactual=(double)(getMax(thmax)+getMax(thmin))/2.0; 
+          return mactual;
+
+        }
+        else{ 
+                if(thmax->n==0){
+                    insert(thmax,data,0);
+                    mactual=data; 
+                    return mactual;
+                }
+                else{
+                     mactual=(double)(getMax(thmax)+getMax(thmin))/2.0;
+                     if(data<mactual){
+                          mactual=getMax(thmax); 
+                          return mactual;
+                     }
+                     else{
+                        insert(thmin,data,1);
+                        mactual= getMax(thmin); 
+                         return mactual;
+                     }
+                }
+        }
+    }
+
   return -1;
-} 
-
-
+}
 #endif 
